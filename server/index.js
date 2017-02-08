6253b5a1519b015310fdba6636f64974c9ee59e4
MrLott <nexriz@hotmail.com>
2017-02-09 00:17:07 +0100

    ejected app so i could add stage-0

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 import express from 'express'
 
-const PORT = 3000
+import morgan from 'morgan'
+
+
+const PORT = 8080
 const app = express()
+
+
+
+// middleware
+app.use(morgan('dev'))
+
+
+// Routes
 
 app.get('/*', (req, res) => {
 	res.send('hello')
 })
 
-app.listen(PORT, () => console.log('Listening on port 3000!'))
+
+
+app.listen(PORT, () => console.log('Listening on port 8080!'))
